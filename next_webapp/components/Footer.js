251fb3bdee5c8e12ef8d87076de725c407485fb3
Siddhant Patil <shimpli.patil2804@gmail.com>
2023-02-09 23:24:22 +0530

    adding Footer

--- next_webapp/components/Footer.js
+++ next_webapp/components/Footer.js
@@ -5,5 +5,14 @@ export default function Footer() {
   const text =
     "Startic Field is a company which aims to endow student entrepreneurs with the power of community, knowledge, capital and mentorship. It is also a forum where we bring together working professionals as contributors who have a passion to startup, years of experience and small amounts of capital but do not have the bandwidth to work on the ground.";
 
-  return <></>;
+  return (
+    <>
+      <footer>
+        <p class={styles.copyrightText}>
+          Copyright &copy; 2023. All Rights Reserved by
+          <a href="#"> GOSS</a>.
+        </p>
+      </footer>
+    </>
+  );
 }
